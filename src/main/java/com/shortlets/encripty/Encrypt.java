e38e554f79788c743d4490af242311a393364aba
fssantos <fssantos>
2016-12-15 11:51:30 -0200

    classes novas

---
+++ src/main/java/com/shortlets/encripty/Encrypt.java
@@ -0,0 +1,81 @@
+package com.shortlets.encripty;
+
+import java.security.spec.KeySpec;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
+public class Encrypt {
+    private static final String UNICODE_FORMAT = "UTF8";
+    public static final String DESEDE_ENCRYPTION_SCHEME = "DESede";
+    private KeySpec ks;
+    private SecretKeyFactory skf;
+    private Cipher cipher;
+    byte[] arrayBytes;
+    private String sgo_encryptionKey;
+    private String sgo_encryptionScheme;
+    SecretKey key;
+    
+    public Encrypt() throws Exception {
+        sgo_encryptionKey = "ForcasdeVanderWaals"
+                + "EasomaDeTodasForcasAtrativasOuRepulsivas"
+                + "QueNaoSejamForcasDevidasALigacoesCovalentes"
+                + "EntreMoleculas";
+                          
+        sgo_encryptionScheme = DESEDE_ENCRYPTION_SCHEME;
+        arrayBytes = sgo_encryptionKey.getBytes(UNICODE_FORMAT);
+        ks = new DESedeKeySpec(arrayBytes);
+        skf = SecretKeyFactory.getInstance(sgo_encryptionScheme);
+        cipher = Cipher.getInstance(sgo_encryptionScheme);
+        key = skf.generateSecret(ks);
+    }
+    
+    public String encrypt(String unencryptedString) {
+        String encryptedString = null;
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
+            byte[] encryptedText = cipher.doFinal(plainText);
+            encryptedString = new String(Base64.encodeBase64URLSafeString(encryptedText));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedString;
+    }
+    public String decrypt(String encryptedString) {
+        String decryptedText=null;
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] encryptedText = Base64.decodeBase64(encryptedString);
+            byte[] plainText = cipher.doFinal(encryptedText);
+            decryptedText= new String(plainText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return decryptedText;
+    }
+   /**
+    * Caso de teste 
+    */
+   public static void main(String args []) throws Exception
+    {
+        
+	   
+	Encrypt td= new Encrypt();
+        String url = "http://d-app01/sgop/app/comunicacaoPortal/validarEmailMgp?pUrl=";
+      
+        String target="validarEmail?numeroComunicacao=500613&codigoValidador=701";
+        String pUrl="documento=201600000221";
+        String encrypted=td.encrypt(pUrl);
+        String decrypted=td.decrypt(encrypted);
+
+        System.out.println("Query String Encriptado: "+ url);
+        System.out.println("URL formada:" + url +encrypted);
+        System.out.println("Query String Desencriptado:"+ url  + decrypted);
+
+    }
+
+}
+
